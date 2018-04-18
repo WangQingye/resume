0464a6d8c0716a2838a60389415555ae547deb0f
WangQingye <329103586@qq.com>
2018-04-18 18:06:08 +0800

    修改小错

--- webpack-dist.config.js
+++ webpack-dist.config.js
@@ -100,7 +100,7 @@ module.exports = {
       allChunks: true,
     }),
     new EndWebpackPlugin(async () => {
-      自定义域名
+      // 自定义域名
       fs.writeFileSync(path.resolve(outputPath, 'CNAME'), 'wqy.fun/resume');
 
       await publishGhPages();
